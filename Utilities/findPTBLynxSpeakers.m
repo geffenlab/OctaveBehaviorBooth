94121b20b6449b6804c85bf2400f2b58eaf3f768
behaviour7 <geffenlab@gmail.com>
2018-08-14 17:26:12 -0400

    tool for chris's behavior project

---
+++ Utilities/findPTBLynxSpeakers.m
@@ -0,0 +1,29 @@
+function devices = findPTBLynxSpeakers()
+% devices = findPTBLynxSpeakers()
+% 
+% Finds speakers that use the Windows DirectSound API. Returns a struct 
+% array with the same formatting as 'PsychPortAudio('GetDevices')';
+%
+% xd  8/14/18  wrote it
+
+% Search for speaker devices by finding devices with the word 'Speakers' in 
+% their name.
+devs = PsychPortAudio('GetDevices');
+speakerIdx = cellfun(@(X)~isempty(strfind(X,'Speakers')) && ~isempty(strfind(X,'Lynx')),...
+  {devs(:).DeviceName},'UniformOutput',false);
+speakerIdx = find(cell2mat(speakerIdx));
+
+% Find devices that use the Windows DirectSound API. This is the one we 
+% want to use to control the sound cards. The other ones have odd artifacts
+% when I tested them...
+audioAPIs = unique({devs(:).HostAudioAPIName});
+disp(audioAPIs);
+selectedAPI = 'Windows DirectSound';
+apiIdx = cellfun(@(X) ~isempty(strfind(X,selectedAPI)),{devs(:).HostAudioAPIName},'UniformOutput',false);
+apiIdx = find(cell2mat(apiIdx));
+
+% Keep only intersection between both lists, i.e. speakers using DirectSound.
+speakerIdx = intersect(speakerIdx,apiIdx);
+devices = devs(speakerIdx);
+
+end
